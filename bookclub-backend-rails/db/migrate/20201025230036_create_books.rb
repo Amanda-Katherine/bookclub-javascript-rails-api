d17cd102e8332b53e85c80a50c9eff9f11d1452b
Amanda-Katherine <walker.akw@gmail.com>
2020-10-25 19:03:00 -0400

    create Book resource

---
+++ bookclub-backend-rails/db/migrate/20201025230036_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :cover_url
+      t.string :preview
+
+      t.timestamps
+    end
+  end
+end
