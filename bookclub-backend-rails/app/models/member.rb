1cb9df8a427b93005e96a64705ddfbe95212b0df
Amanda-Katherine <walker.akw@gmail.com>
2020-10-26 12:44:54 -0400

    edit Class relationships

--- bookclub-backend-rails/app/models/member.rb
+++ bookclub-backend-rails/app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ApplicationRecord
-    has_many :books, through: :book_groups #CONFRIM RELATIONSHIP
+    # has_many :books, through: :book_groups #CONFRIM RELATIONSHIP
     belongs_to :book_group #has many, belongs to
-    has_many :gatherings, through: :book_groups #confirm this relationship
+    # has_many :gatherings, through: :book_groups #confirm this relationship
 
     has_secure_password
 end
