c030623ad9a1146ed3542923fcf8e543d18da64f
Amanda-Katherine <walker.akw@gmail.com>
2020-10-26 23:45:44 -0400

    update Book class relationships

--- bookclub-backend-rails/app/models/book.rb
+++ bookclub-backend-rails/app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
     has_many :book_groups #many to many
     has_many :gatherings#, through: :book_groups #has many through
-    has_many :members, through: :book_groups #CONFRIM RELATIONSHIP
+    # has_many :members, through: :book_groups #CONFRIM RELATIONSHIP
 end
