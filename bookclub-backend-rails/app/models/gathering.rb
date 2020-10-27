561c70f99b8f6b68dc3af31bd76cd89daddb88f7
Amanda-Katherine <walker.akw@gmail.com>
2020-10-26 23:45:21 -0400

    update Gathering class relationship

--- bookclub-backend-rails/app/models/gathering.rb
+++ bookclub-backend-rails/app/models/gathering.rb
@@ -1,5 +1,5 @@
 class Gathering < ApplicationRecord
   belongs_to :book_group #has many, belongs to
   belongs_to :book #through: :book_groups #confirm this relationship...through book groups?
-  has_many :members, through: :book_groups #confirm this relationship
+  # has_many :members, through: :book_groups #confirm this relationship
 end
