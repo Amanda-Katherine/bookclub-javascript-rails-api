29435500f60b96b5c9757080a8c163f43ca17fa9
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 02:36:18 -0400

    add eventListener to fetch list of BookGroup names

--- bookclub-frontend-javascript/src/index.js
+++ bookclub-frontend-javascript/src/index.js
@@ -4,4 +4,5 @@ let appContainer = document.querySelector('main')
 appContainer.setAttribute("class", "suggestions-container")
 
 document.getElementById('search-button').addEventListener('click', Suggestion.fetchSuggestionOptions, false)
+document.getElementById('select-club').addEventListener('click', BookGroup.fetchClubNames, false)
 
