3ce7f2712b828b9b10ed978a3c1171cd4c2d6bef
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 02:27:58 -0400

    edit property names

--- bookclub-backend-rails/db/migrate/20201025230036_create_books.rb
+++ bookclub-backend-rails/db/migrate/20201025230036_create_books.rb
@@ -3,8 +3,8 @@ class CreateBooks < ActiveRecord::Migration[6.0]
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.string :cover_url
-      t.string :preview
+      t.string :image
+      t.string :description
 
       t.timestamps
     end
