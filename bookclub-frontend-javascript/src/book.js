cbabcbbfdcd78cebb44dcebf4ed6292e13b55548
Amanda-Katherine <walker.akw@gmail.com>
2020-10-27 23:32:03 -0400

    create Book class

---
+++ bookclub-frontend-javascript/src/book.js
@@ -0,0 +1,47 @@
+    static allBooks = []
+
+    constructor(book) {
+        debugger
+        // this.title = book.
+        // this.author = book.
+        // this.cover_url = book.
+        // this.preview = book.
+        // Book.allBooks.push(this)  
+    }
+
+    static createBook(bookInfo) {    
+        let title = bookInfo.title
+
+    //    (bookInfo.authors) ? let author = bookInfo.authors[0] : let author = "No known author"
+           
+        // if (!!bookInfo.authors) {
+            let author = bookInfo.authors[0]
+        // } else {
+        //     let author = "No known author"
+        // }
+
+        // if (!!bookInfo.description) {
+            let description = bookInfo.description
+        // } else {
+        //     let description = "No preview given"
+        // }
+
+        let image = bookInfo.imageLinks.thumbnail
+
+        let book = {book: {title, author, description, image}}
+        
+        let options = {
+            method: "POST",
+            headers: {"Content-Type": "application/json", "Accept": "application/json"},
+            body: JSON.stringify(book)
+        }
+        let r = document.getElementsById("search-button").value = "" 
+      
+        debugger
+
+    }
+
+    
+
+    
+
