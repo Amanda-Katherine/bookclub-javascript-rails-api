92478ec45db80f00e067778d68ca1f8919619847
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 02:27:47 -0400

    edit attribute names

--- bookclub-backend-rails/app/serializers/book_serializer.rb
+++ bookclub-backend-rails/app/serializers/book_serializer.rb
@@ -1,4 +1,4 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :cover_url, :preview
+  attributes :id, :title, :author, :image, :description
   has_many :gatherings
 end
