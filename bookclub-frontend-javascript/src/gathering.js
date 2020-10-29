f6bab317612b92e355a4be5634a253aeae65cfc9
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 23:54:54 -0400

    build constructor

--- bookclub-frontend-javascript/src/gathering.js
+++ bookclub-frontend-javascript/src/gathering.js
@@ -1,6 +1,12 @@
 class Gathering {
+    static allGatherings = []
 
-    constructor() {
-        
+    constructor(gathering) {
+        this.id = gathering.id
+        this.host = gathering.host
+        this.date = gathering.date
+        this.address = gathering.address
+        this.time = gathering.time
+        Gathering.allGatherings.push(this)
     }
 }
