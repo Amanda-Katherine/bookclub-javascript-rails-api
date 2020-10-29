41958794fc2a486840076169eb500cb206b064d4
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 23:44:23 -0400

    add suggestion relationship and fix typo

--- bookclub-backend-rails/app/serializers/book_serializer.rb
+++ bookclub-backend-rails/app/serializers/book_serializer.rb
@@ -1,5 +1,6 @@
 class BookSerializer < ActiveModel::Serializer
   attributes :id, :title, :author, :image, :description
   has_many :gatherings
-  has_many :books
+  has_many :book_groups
+  has_many :suggestions
 end
