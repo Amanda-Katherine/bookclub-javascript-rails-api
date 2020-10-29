ea45be7610aa70bd307d408fe0b1253c3b7a120f
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 23:55:20 -0400

    build constructor

--- bookclub-frontend-javascript/src/member.js
+++ bookclub-frontend-javascript/src/member.js
@@ -1,6 +1,11 @@
 class Member {
+    static allMembers = []
 
-    constructor() {
-        
+    constructor(member) {
+        this.id = member.id
+        this.first_name = member.first_name
+        this.last_name = member.last_name
+        this.book_group_id = member.book_group_id
+        Member.allMembers.push(this)
     }
 }
