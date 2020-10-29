c7e50145dd07381c0e3be3a95f4e2a51a778749b
Amanda-Katherine <walker.akw@gmail.com>
2020-10-28 23:55:13 -0400

    update base HTML

--- bookclub-frontend-javascript/src/index.js
+++ bookclub-frontend-javascript/src/index.js
@@ -1,7 +1,12 @@
 const BACKEND_URL = "http://localhost:3000";
 
 let appContainer = document.querySelector('main')
-appContainer.setAttribute("class", "suggestions-container")
+let sugContainer = document.createElement('div')
+let grpContainer = document.createElement('div')
+sugContainer.setAttribute("class", "suggestions-container")
+grpContainer.setAttribute("class", "group-container")
+
+
 
 document.getElementById('search-button').addEventListener('click', Suggestion.fetchSuggestionOptions, false)
 document.getElementById('select-club').addEventListener('click', BookGroup.fetchClubNames.bind(BookGroup), false)
