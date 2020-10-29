2ad561c5a7c124a0d1929f5e2f7634d81b7884c0
Amanda-Katherine <walker.akw@gmail.com>
2020-10-29 03:23:24 -0400

    create strong params and create action

--- bookclub-backend-rails/app/controllers/suggestions_controller.rb
+++ bookclub-backend-rails/app/controllers/suggestions_controller.rb
@@ -2,5 +2,22 @@ class SuggestionsController < ApplicationController
     def index
         suggestions = Suggestion.all
         render json: suggestions
+    end
+
+    def create
+      suggestion = Suggestion.new(suggestion_params)
+
+      if suggestion.save
+        render json: suggestion
+      else
+        render json: {message:comment.errors.full_messages}
       end
+    end
+
+
+    private
+
+    def suggestion_params
+        params.require(:suggestion).permit(:vote, :book_id, :book_group_id)
+    end
 end
