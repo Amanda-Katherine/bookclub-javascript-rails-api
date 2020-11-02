31290641cbe3fe5689c15fe3ec27e9bda7855d98
Amanda-Katherine <walker.akw@gmail.com>
2020-11-01 23:29:13 -0500

    update for used suggestion routes only

--- bookclub-backend-rails/config/routes.rb
+++ bookclub-backend-rails/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :suggestions
-  resources :gatherings
+  resources :suggestions, only: [:index, :create, :update, :show, :destroy]
+  resources :gatherings, only: [:index]
   resources :members
   resources :book_groups
   resources :books
