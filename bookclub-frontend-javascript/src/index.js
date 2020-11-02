f887093d1736609b97cb152858479e969d02ee65
Amanda-Katherine <walker.akw@gmail.com>
2020-11-01 20:11:24 -0500

    add gathering container and class attribute

--- bookclub-frontend-javascript/src/index.js
+++ bookclub-frontend-javascript/src/index.js
@@ -1,15 +1,18 @@
 const BACKEND_URL = "http://localhost:3000";
 
 let appContainer = document.querySelector('main')
+let gatheringContainer = document.createElement('div')
 let grpContainer = document.createElement('div')
 let sugContainer = document.createElement('div')
 let possibleSugs = document.createElement('div')
 let grpSugs = document.createElement('div')
 let search = document.getElementById("search-container")
 
+gatheringContainer.setAttribute("class", "gathering-container")
 grpContainer.setAttribute("class", "group-container")
 sugContainer.setAttribute("class", "suggestions-container")
 appContainer.append(grpContainer)
+appContainer.append(gatheringContainer)
 
 document.getElementById('search-button').addEventListener('click', Suggestion.fetchSuggestionOptions, false)
 document.getElementById('select-club').addEventListener('click', BookGroup.fetchClubNamesandBooks.bind(BookGroup), {once: true})
