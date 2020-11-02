fbb36a141b89f69edda7cc3bcaa4506eacf02a33
Amanda-Katherine <walker.akw@gmail.com>
2020-11-01 23:29:33 -0500

    update for used routes only for BookGroups

--- bookclub-backend-rails/config/routes.rb
+++ bookclub-backend-rails/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :suggestions, only: [:index, :create, :update, :show, :destroy]
   resources :gatherings, only: [:index]
   resources :members
-  resources :book_groups
   resources :books
-  # get '/test', to: 'application#test'
+  resources :book_groups, only: [:index]
 end
