c0bf5134aaab26c2f9d98ca62ebfa911c7a98c40
Amanda-Katherine <walker.akw@gmail.com>
2021-02-17 23:51:22 -0500

    make create action with conditional if new member was saved or not

--- bookclub-backend-rails/app/controllers/members_controller.rb
+++ bookclub-backend-rails/app/controllers/members_controller.rb
@@ -3,4 +3,23 @@ class MembersController < ApplicationController
         members = Member.all
         render json: members
     end
+
+    def create 
+        member = Member.new(member_params)
+        binding.pry
+
+        if member.save
+            session[:member_id] = member.id
+            render json: {
+                status: :created,
+                logged_in: true,
+                member: member
+            }
+        else
+            render json: {
+                status: 403,
+                logged_in: false
+            }
+        end
+    end
 end
