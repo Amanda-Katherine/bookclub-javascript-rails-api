6dae279b769c340913f229cbe830202c5882fdcb
Amanda-Katherine <walker.akw@gmail.com>
2021-02-17 23:59:00 -0500

    create registration/login form elements

---
+++ bookclub-frontend-javascript/src/login_registration_form.js
@@ -0,0 +1,8 @@
+function loadRegistrationLogin() {
+  // create the elements
+  var div = document.createElement("div"),
+    log = document.createElement("div"),
+    reg = document.createElement("div"),
+    loginForm = document.createElement("form"),
+    registerForm = document.createElement("form");
+}
