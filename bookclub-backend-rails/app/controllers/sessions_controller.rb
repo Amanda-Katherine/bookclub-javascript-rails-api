73172f58ed57f41c06ee90396457e110bcd7df06
Amanda-Katherine <walker.akw@gmail.com>
2021-02-17 23:54:31 -0500

    create login and logout routes

--- bookclub-backend-rails/app/controllers/sessions_controller.rb
+++ bookclub-backend-rails/app/controllers/sessions_controller.rb
@@ -1,3 +1,8 @@
+#this page created from customer user auth documentation (not used with Devise)
+class SessionsController < ApplicationController
+    include CurrentMemberConcern
+    # skip_before_action :verify_authenticity_token
+
     def create
         binding.pry
         member = Member
@@ -15,3 +20,33 @@
             render json: { status: 401 }
         end
     end
+
+    def logged_in?
+        if @current_member
+            render json: {
+                logged_in: true, 
+                member: @current_member
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
+
+    def destroy # was called logout for custom user auth
+        # reset_session # from custom user auth method
+        session[:member_id] = nil
+        # session.delete(:user_id)  #test this if above doesn't work
+
+        render json: { status: 200, logged_out: true }
+    end
+end
+
+#use the below code in your terminal to test if the session will accept a POST request 
+#step one: start the rails s backend server
+#step two: copy and paste the below into the terminal and run it (make sure you've cd'ed into the backend main directory)
+# curl --header "Content-Type: application/json" \
+# > --request POST \
+# > --data '{"member": {"email": "test@test.com", "password": "pass"}}' \
+# > http://localhost:3000/sessions
--- bookclub-backend-rails/config/routes.rb
+++ bookclub-backend-rails/config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :members
   resources :book_groups, only: [:index]
   resources :books, only: [:index, :create]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
