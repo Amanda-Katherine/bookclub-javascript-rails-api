8e23462297c3232aa4722df5e0843097e1ac4e9a
Amanda-Katherine <walker.akw@gmail.com>
2021-02-20 02:02:49 -0500

    add in member email and book group default

--- bookclub-backend-rails/db/migrate/20201026002438_create_members.rb
+++ bookclub-backend-rails/db/migrate/20201026002438_create_members.rb
@@ -3,8 +3,9 @@ class CreateMembers < ActiveRecord::Migration[6.0]
     create_table :members do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email
       t.string :password_digest
-      t.references :book_group, null: false, foreign_key: true
+      t.references :book_group, null: false, foreign_key: true, default: 0
 
       t.timestamps
     end
