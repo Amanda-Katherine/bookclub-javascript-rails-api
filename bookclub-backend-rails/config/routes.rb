e19f8dec9fb354c2065adbc1ccf1e1e5c993e655
Amanda-Katherine <walker.akw@gmail.com>
2021-02-20 02:01:57 -0500

    update available routes for members and sessions

--- bookclub-backend-rails/config/routes.rb
+++ bookclub-backend-rails/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :suggestions, except: [:new, :edit]
   resources :gatherings, only: [:index]
-  resources :members
+  resources :members, only: [:create, :index, :show]
   resources :book_groups, only: [:index]
   resources :books, only: [:index, :create]
+  # resources :registrations, only: [:create]
+  resources :sessions, only: [:create]
+  
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   root to: "static#home"
