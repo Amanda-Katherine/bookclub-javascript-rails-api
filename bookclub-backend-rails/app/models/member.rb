9c8251447ef405d86945ea0f5c446e0b2d06940c
Amanda-Katherine <walker.akw@gmail.com>
2021-02-20 01:51:11 -0500

    add in member validations for email and password

--- bookclub-backend-rails/app/models/member.rb
+++ bookclub-backend-rails/app/models/member.rb
@@ -3,4 +3,11 @@ class Member < ApplicationRecord
 
   has_secure_password
 
+  validates :email, length: { minimum: 4 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
 end
+
