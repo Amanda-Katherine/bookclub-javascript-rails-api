528a9da11b204a24fc7583a0d0cff5ae2f43103c
Amanda-Katherine <walker.akw@gmail.com>
2021-02-20 22:38:35 -0500

    add id attribute to appContainer

--- bookclub-frontend-javascript/src/index.js
+++ bookclub-frontend-javascript/src/index.js
@@ -11,11 +11,13 @@ let navLogin = document.querySelector("#nav-login");
 let navLogout = document.querySelector("#nav-logout");
 let navBookClubs = document.querySelector("#nav-select-club");
 
-gatheringContainer.setAttribute("class", "gathering-container")
-grpContainer.setAttribute("class", "group-container")
-sugContainer.setAttribute("class", "suggestions-container")
-appContainer.append(grpContainer)
-appContainer.append(gatheringContainer)
+appContainer.setAttribute("id", "app-container");
+gatheringContainer.setAttribute("class", "gathering-container");
+grpContainer.setAttribute("class", "group-container");
+grpContainer.setAttribute("id", "book-group-container");
+sugContainer.setAttribute("class", "suggestions-container");
+sugContainer.setAttribute("id", "suggestions-group-container");
+search.setAttribute("id", "search-container");
 
 document.getElementById('search-button').addEventListener('click', Suggestion.fetchSuggestionOptions, false)
 document.getElementById('select-club').addEventListener('click', BookGroup.fetchClubNamesandBooks.bind(BookGroup), {once: true})
