20d81db61b1a9bcc46bf0dd69f96445c68633f18
Amanda-Katherine <walker.akw@gmail.com>
2021-02-20 22:38:10 -0500

    declare nav bar variables

--- bookclub-frontend-javascript/src/index.js
+++ bookclub-frontend-javascript/src/index.js
@@ -1,12 +1,15 @@
 const BACKEND_URL = "http://localhost:3000";
 
-let appContainer = document.querySelector('main')
-let gatheringContainer = document.createElement('div')
-let grpContainer = document.createElement('div')
-let sugContainer = document.createElement('div')
-let possibleSugs = document.createElement('div')
-let grpSugs = document.createElement('div')
-let search = document.getElementById("search-container")
+let appContainer = document.querySelector("main");
+let gatheringContainer = document.createElement("div");
+let grpContainer = document.createElement("div");
+let sugContainer = document.createElement("div");
+let possibleSugs = document.createElement("div");
+let grpSugs = document.createElement("div");
+let search = document.getElementById("search-container");
+let navLogin = document.querySelector("#nav-login");
+let navLogout = document.querySelector("#nav-logout");
+let navBookClubs = document.querySelector("#nav-select-club");
 
 gatheringContainer.setAttribute("class", "gathering-container")
 grpContainer.setAttribute("class", "group-container")
